f5840566738c870284e06b9ea140aa7a19998ba7
gavin <gavin.lim@alumni.ubc.ca>
2021-12-20 00:43:54 -0800

    Add palette

--- script.js
+++ script.js
@@ -7,36 +7,22 @@ let black = true;
 let gradient = ['#FE2712', '#FC500A', '#FB9902', '#FCCC1A',
                 '#FEFE33', '#B2D732', '#66B032', '#347C98',
                 '#0247FE', '#4424D6', '#8601AF', '#C21460']
+
 var gradient_idx = 0;
 
 const inc_color = () => {
-    // var hex = parseInt(color.slice(1), 16) + 1;
-    // console.log(hex);
-    // return '#' + hex.toString(16);
-
-    // let color_arr = color.slice(
-    //     color.indexOf("(") + 1, 
-    //     color.indexOf(")")
-    // ).split(",");
-
-    // let R = color_arr[0];
-    // let G = color_arr[1];
-    // let B = color_arr[2];
-    // console.log(gradient_idx, gradient[gradient_idx], gradient.length);
-    
-    let color = gradient[gradient_idx];
-    if (gradient_idx == gradient.length) {
-        gradient_idx = 0;
-    } else {
-        gradient_idx++;
-    }
-
-    return color;
+    gradient_idx++
+    if (gradient_idx == gradient.length) {gradient_idx = 0;}
+    return gradient[gradient_idx];
 }
 
 // Initiate drawing when mouse pressed
-function mouse_down() {
+function mouse_click() {
     started = true;
+    if (black == false) {
+        curr_color = inc_color(curr_color);
+    } 
+    this.style.setProperty('background-color', curr_color);
 }
 
 // Change background color of cube
@@ -50,32 +36,19 @@ function mouse_move(e) {
 }
 
 // Disable drawing when mouse released
-function mouse_up() {
+function mouse_release() {
     started = false;
 }
 
-function update() {
-    // var select = document.getElementById('size');
-    // num_cubes = select.options[select.selectedIndex].value;
-
-    // Slider range
-    var slider = document.getElementById('cube_range');
-    var display = document.getElementById('display');
-    display.innerHTML = slider.value + "x" + slider.value; 
-    num_cubes = slider.value;
-
-    slider.oninput = function() {
-        display.innerHTML = this.value + "x" + this.value;
-        num_cubes = this.value;
-    }
-
-    // Remove existing grid
+// Remove existing grid
+function clear_grid() { 
     const parent = document.getElementsByClassName('grid-container')[0];
-    while (parent.firstChild) {
-        parent.removeChild(parent.lastChild);
-    }
+    parent.innerHTML = "";
+}
 
+function add_grid(num_cubes) {
     // Generate new grid
+    const parent = document.getElementsByClassName('grid-container')[0];
     for (let i = 0; i < (num_cubes * num_cubes); i++) {
         const cube = document.createElement('div'); 
         cube.classList.add('cube');
@@ -91,31 +64,62 @@ function update() {
     var cubes = document.getElementsByClassName('cube');
     for (var i = 0; i < cubes.length; i++) {
         cubes[i].style.width = cube_sz;
-        cubes[i].addEventListener('mousedown', mouse_down, false);
-        cubes[i].addEventListener('mousemove', mouse_move, false); 
-        cubes[i].addEventListener('mouseup', mouse_up, false);
+        cubes[i].addEventListener('mousedown', mouse_click, false);
+        cubes[i].addEventListener('mouseover', mouse_move, false); 
+        cubes[i].addEventListener('mouseup', mouse_release, false);
         cubes[i].addEventListener('click', e => {
             e.currentTarget.style.setProperty('background-color', curr_color);
         });
-    }    
+    }  
 }
 
-// Init reset button
+function update() {
+    // Slider range
+    var slider = document.getElementById('cube_range');
+    var display = document.getElementById('display');
+    display.innerHTML = slider.value + ' x ' + slider.value; 
+    num_cubes = slider.value;
+
+    slider.oninput = function() {
+        display.innerHTML = this.value + ' x ' + this.value;
+        num_cubes = this.value;
+    }
+
+    clear_grid();
+    add_grid(num_cubes);  
+}
+
+// Initialize reset button
 const retry = document.getElementById('reset');
 retry.addEventListener('click', update, false);
 
-// Init color toggle button
+// Initialize color toggle button
 const color = document.getElementById('color-toggle');
 color.addEventListener('click', e => {
     if (black == true) {
         e.currentTarget.innerHTML = 'Rainbow Mode';
         black = false;
-        curr_color = inc_color();
     } else {
         e.currentTarget.innerHTML = 'Black Mode';
         black = true;
         curr_color = '#333'
     }
+});
+
+// Initialize color buttons
+const palette = document.getElementsByClassName('palette')[0];
+gradient.forEach(color => {
+    const color_btn = document.createElement('button');
+
+    // Add attributes and properties
+    color_btn.classList.add('color-button');
+    color_btn.style.setProperty('background-color', color);
+    color_btn.setAttribute('id', color);
+    color_btn.addEventListener('click', function() {
+        curr_color = this.id;
+        black = true;
+    }, false);
+    palette.appendChild(color_btn);
 });
 
 update();
